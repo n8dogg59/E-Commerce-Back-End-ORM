e47ec95f245554ac79368e8f9ef457ab7b3304bb
Nate Schroeder <n8dogg59@hotmail.com>
2021-03-20 21:29:59 -0500

    finished api routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,14 +5,63 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: [
+          'id',
+          'category_name'
+        ]
+      },
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({message: 'No product found with this id'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
@@ -35,6 +84,7 @@ router.post('/', (req, res) => {
             tag_id,
           };
         });
+        console.log(productTagIdArr);
         return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
@@ -90,7 +140,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then((dbProductData) => {
+    if (!dbProductData) {
+      res.status(404).json({message: 'No post found with this id'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# E-commerce Back End Starter Code
+# E-commerce Back End 
+
+## Description
+This project utilizes Sequelize to interact with a MySQL database to build the back end for an e-commerce site.
+
+## Features
+* The user can enter there database name, MySQL username, and MySQL password to the the environment variable file and connect to the database using Sequelize.
+* When the user enters the schema and seed commands a development database is created and seeded with the test data.
+* When the user starts the server the Sequalize models are synced to the MySQL database.
+* The user can use Insomnia Core to open API Get routes for categories, products, and tags and view the corresponding requested data for these routes in JSON format.
+* The user can use Insomnia Core to test API Post, PUT, and Delete routes and successfully create, update, and delete data for either the categories, products, or tags.
+
+
+## Demonstration Video
+To view a demonstration of how this application works please click on this YouTube link https://youtu.be/ZzW3PjwwPKU.
+
+## Built With
+* MySQL
+* Sequelize
+* Express
+* JavaScript
+* dotenv
+
+## Contributing
+Nate Schroeder
+
+## Github Repository
+https://github.com/n8dogg59/employee-tracker
+
+
+
